58791a7277e78031460f658ce4fe0da067d19c67
Milana Evtifeeva <evtifeevama@mer.ci.nsu.ru>
2024-03-12 01:13:37 +0700

    Add StatisticsEmployeeViewModel

--- EvtifeevaShop/EvtifeevaShop/Models/View/Admin/StatisticsEmployeeModel.cs
+++ EvtifeevaShop/EvtifeevaShop/Models/View/Admin/StatisticsEmployeeModel.cs
@@ -1,6 +1,7 @@
 ﻿using EvtifeevaShop.Models.Database;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace EvtifeevaShop.Models.View.Admin
 {
    public class StatisticsEmployeeModel
     {
-		//public DataTable<EmployeeTable> {get;set;}
+		public DataTable EmployeeTable  {get;set;}
 		public List<Position> PositionList { get; set; }
 		public string Selected { get; set; }
 		public string Search { get; set; }
---
+++ EvtifeevaShop/EvtifeevaShop/ViewModels/Admin/StatisticsEmployeeViewModel.cs
@@ -0,0 +1,65 @@
+﻿using EvtifeevaShop.Models.Database;
+using EvtifeevaShop.Models.View.Admin;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvtifeevaShop.ViewModels.Admin
+{
+    public class StatisticsEmployeeViewModel: ViewModelBase
+    {
+		private StatisticsEmployeeModel _StatisticsEmployeeModel = new();
+
+		public List<Position> PositionList
+		{
+			get { return _StatisticsEmployeeModel.PositionList; }
+			set
+			{
+				_StatisticsEmployeeModel.PositionList = value;
+				OnPropertyChanged(nameof(PositionList));
+			}
+		}
+
+		public string Selected
+		{
+			get { return _StatisticsEmployeeModel.Selected; }
+			set
+			{
+				_StatisticsEmployeeModel.Selected = value;
+				OnPropertyChanged(nameof(Selected));
+			}
+		}
+
+		public string Search
+		{
+			get { return _StatisticsEmployeeModel.Search; }
+			set
+			{
+				_StatisticsEmployeeModel.Search = value;
+				OnPropertyChanged(nameof(Search));
+			}
+		}
+
+		public bool SortAsc
+		{
+			get { return _StatisticsEmployeeModel.SortAsc; }
+			set
+			{
+				_StatisticsEmployeeModel.SortAsc = value;
+				OnPropertyChanged(nameof(SortAsc));
+			}
+		}
+		public DataTable EmployeeTable
+		{
+			get { return _StatisticsEmployeeModel.EmployeeTable; }
+			set
+			{
+				_StatisticsEmployeeModel.EmployeeTable = value;
+				OnPropertyChanged(nameof(EmployeeTable));
+			}
+		}
+	}
+}
