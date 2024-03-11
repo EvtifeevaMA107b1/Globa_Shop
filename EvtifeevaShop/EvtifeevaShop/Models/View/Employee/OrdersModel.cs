a8e09bec5a0c0ec2571e53116bc308c9d5db9dd2
Milana Evtifeeva <evtifeevama@mer.ci.nsu.ru>
2024-03-12 01:36:08 +0700

    Add view model orders for employee

--- EvtifeevaShop/EvtifeevaShop/Models/View/Employee/OrdersModel.cs
+++ EvtifeevaShop/EvtifeevaShop/Models/View/Employee/OrdersModel.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EvtifeevaShop.Models.View.Employee
 {
-    class OrderModel
+    class OrdersModel
     {
-		//public DataTable<OrderTable> { get; set; }
+		public DataTable OrderTable { get; set; }
 
 		public string PickupPointAddress {  get; set; }
 		public string CountOrders { get; set;}
---
+++ EvtifeevaShop/EvtifeevaShop/ViewModels/Employee/OrdersViewModel.cs
@@ -0,0 +1,44 @@
+﻿using EvtifeevaShop.Models.View;
+using EvtifeevaShop.Models.View.Employee;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvtifeevaShop.ViewModels.Employee
+{
+    public class OrdersViewModel: ViewModelBase
+    {
+		private OrdersModel _OrdersModel= new();
+		public DataTable OrderTable
+		{
+			get { return _OrdersModel.OrderTable; }
+			set
+			{
+				_OrdersModel.OrderTable = value;
+				OnPropertyChanged(nameof(OrderTable));
+			}
+		}
+
+		public string PickupPointAddress
+		{
+			get { return _OrdersModel.PickupPointAddress; }
+			set
+			{
+				_OrdersModel.PickupPointAddress = value;
+				OnPropertyChanged(nameof(PickupPointAddress));
+			}
+		}
+		public string CountOrders
+		{
+			get { return _OrdersModel.CountOrders; }
+			set
+			{
+				_OrdersModel.CountOrders = value;
+				OnPropertyChanged(nameof(CountOrders));
+			}
+		}
+	}
+}
